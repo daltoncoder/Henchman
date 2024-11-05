607b4d6f93756e721e512fef46a5e47a8cda6306
daltoncoder <daltoncoder2@gmail.com>
2024-11-05 16:03:41 -0500

    prompt generation

--- src/prompts.rs
+++ src/prompts.rs
@@ -1,9 +1,10 @@
+use aho_corasick::AhoCorasick;
 use std::{fs, path::PathBuf};
 
 use serde::{Deserialize, Serialize};
 
 /// Loads prompts for certain situations from a config file so it can easily be swapped out and changed
-#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Default)]
 pub struct Prompts {
     /// Template that takes in: Short term memories, Long term memories, external context(recent notifications and tags on twitter), recent posts, and example tweets to generate the AI's tweet
     pub tweet_template: String,
@@ -14,6 +15,8 @@ pub struct Prompts {
     pub significance_score_template: String,
     /// Template that takes in recent posts that have wallet addresses in them and decides if the AI should take on chain action like transfer them money
     pub wallet_decision_template: String,
+    /// Template that formats or chooses the tweet to post if multiple are in the prompt. Takes in the original prompt as an arg
+    pub formatter_template: String,
     /// List of example tweets we can add to our prompts to give more context
     pub example_tweets: Vec<String>,
 }
@@ -24,18 +27,66 @@ impl Prompts {
         let raw = fs::read_to_string(path).expect("Unable to read prompts.toml");
         toml::from_str(&raw).expect("Unable to parse prompts.toml")
     }
-}
 
-// todo: Should we set defaults here incase the prompts.toml doesnt exist??
-impl Default for Prompts {
-    fn default() -> Self {
-        Self {
-            tweet_template: Default::default(),
-            short_term_memory_template: Default::default(),
-            significance_score_template: Default::default(),
-            wallet_decision_template: Default::default(),
-            example_tweets: Default::default(),
-        }
+    pub fn get_tweet_prompt(
+        &self,
+        short_term_memory: String,
+        long_term_memories: Vec<String>,
+        recent_posts: Vec<String>,
+        external_context: Vec<String>,
+    ) -> String {
+        let patterns = &[
+            "{short_term_memories}",
+            "{long_term_memories}",
+            "{external_context}",
+            "{recent_posts}",
+            "{example_tweets}",
+        ];
+        let replace_with = &[
+            short_term_memory,
+            long_term_memories.join("\n"),
+            external_context.join("\n"),
+            recent_posts.join("\n"),
+            self.example_tweets.join("\n"),
+        ];
+
+        let ac = AhoCorasick::new(patterns).unwrap();
+
+        ac.replace_all(&self.tweet_template, replace_with)
+    }
+
+    pub fn get_short_term_memory_prompt(&self, external_context: Vec<String>) -> String {
+        let ac = AhoCorasick::new(&["{external_context}"]).unwrap();
+        ac.replace_all(
+            &self.short_term_memory_template,
+            &[external_context.join("\n")],
+        )
+    }
+
+    pub fn get_significance_prompt(&self, memory: &str) -> String {
+        let ac = AhoCorasick::new(&["{memory}"]).unwrap();
+
+        ac.replace_all(&self.significance_score_template, &[memory])
+    }
+
+    pub fn get_wallet_decision_prompt(
+        &self,
+        posts: Vec<String>,
+        wallet_matches: Vec<String>,
+        wallet_balance: String,
+    ) -> String {
+        let patterns = &["{posts}", "{matches}", "{wallet_balance}"];
+        let replace_with = &[posts.join("\n"), wallet_matches.join("\n"), wallet_balance];
+
+        let ac = AhoCorasick::new(patterns).unwrap();
+
+        ac.replace_all(&self.wallet_decision_template, replace_with)
+    }
+
+    pub fn get_formatter_prompt(&self, original_prompt: &str) -> String {
+        let ac = AhoCorasick::new(&["{prompt}"]).unwrap();
+
+        ac.replace_all(&self.formatter_template, &[original_prompt])
     }
 }
 
@@ -55,6 +106,75 @@ fn test_our_prompts_toml() {
     println!("Wallet Decision Template:");
     println!("{}", prompts.wallet_decision_template);
 
+    println!("Formatter Template");
+    println!("{}", prompts.formatter_template);
+
     println!("Example Tweets:");
     prompts.example_tweets.iter().for_each(|e| println!("{e}"));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn get_prompts() -> Prompts {
+        Prompts::load("./prompts.toml".into())
+    }
+
+    fn get_vec_of_strings(text: &str) -> Vec<String> {
+        [format!("{text}1"), format!("{text}2"), format!("{text}3")].to_vec()
+    }
+    #[test]
+    fn test_get_tweet_prompt() {
+        let prompts = get_prompts();
+
+        let tweet = prompts.get_tweet_prompt(
+            "This is my short term memory".to_string(),
+            get_vec_of_strings("memory"),
+            get_vec_of_strings("recent"),
+            get_vec_of_strings("context"),
+        );
+
+        println!("{tweet}");
+    }
+
+    #[test]
+    fn test_get_short_memory_prompt() {
+        let prompts = get_prompts();
+
+        let prompt = prompts.get_short_term_memory_prompt(get_vec_of_strings("context"));
+
+        println!("{prompt}");
+    }
+
+    #[test]
+    fn test_get_significance_prompt() {
+        let prompts = get_prompts();
+
+        let prompt = prompts.get_significance_prompt("this is my memory");
+
+        println!("{prompt}");
+    }
+
+    #[test]
+    fn test_get_wallet_decision_prompt() {
+        let prompts = get_prompts();
+
+        let prompt = prompts.get_wallet_decision_prompt(
+            get_vec_of_strings("posts"),
+            get_vec_of_strings("0x12..34"),
+            "69.430".to_string(),
+        );
+
+        println!("{prompt}");
+    }
+
+    #[test]
+    fn test_get_formatter_prompt() {
+        let prompts = get_prompts();
+
+        let prompt = prompts.get_formatter_prompt("This is the original prompt.");
+
+        println!("{prompt}");
+    }
 }
