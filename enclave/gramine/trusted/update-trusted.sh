92d421caee3006f752a2611aad5df07de8506f97
zorvan <nonlinear_dynamic@yahoo.com>
2024-11-09 00:42:57 +0000

    docker enclave fix set 1

--- enclave/gramine/trusted/update-trusted.sh
+++ enclave/gramine/trusted/update-trusted.sh
@@ -5,6 +5,7 @@ mkdir -p ./arch_libdir/
 cp -f /lib/x86_64-linux-gnu/libcrypto.so.3 ./arch_libdir/
 cp -f /lib/x86_64-linux-gnu/libgcc_s.so.1 ./arch_libdir/
 cp -f /lib/x86_64-linux-gnu/libssl.so.3 ./arch_libdir/
+cp -f /lib/x86_64-linux-gnu/libstdc++.so.6 ./arch_libdir/
 
 mkdir -p ./gramine/runtime/glibc/
 cp -f /usr/lib/x86_64-linux-gnu/gramine/libsysdb.so ./gramine/
